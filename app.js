99b13cede2afac4cacb5035a16947eceb6685441
Harsh Foujdar <harshbtp738@gmail.com>
2026-01-08 14:47:21 +0530

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
+app.use(express.static(path.join(__dirname, "public")));
 // view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "pages"));
